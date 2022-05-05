fe26e591583545fb17ebff9ff0917cedeacc2637
Roman Pardo <atlanticpr90@gmail.com>
2022-05-04 23:30:58 -0500

    Se crea clase Bisicleta y sus campos

---
+++ PracticaGIT/PracticaGIT/Bicicleta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaGIT
+{
+    public class Bicicleta
+    {
+        public string marca;
+        public string modelo;
+        public int rodada;
+        public int velocidades;
+        public string color;
+    }
+}
