4a03c28b88ff4f5715b56051992e5030ed370e03
Roman Pardo <atlanticpr90@gmail.com>
2022-05-04 23:41:52 -0500

    Se agrergo método a la clase bicicleta

--- PracticaGIT/PracticaGIT/Bicicleta.cs
+++ PracticaGIT/PracticaGIT/Bicicleta.cs
@@ -13,5 +13,16 @@ namespace PracticaGIT
         public int rodada;
         public int velocidades;
         public string color;
+
+        public void informacion()
+        {
+            Console.WriteLine("\nInformacion de la bicicleta");
+            Console.WriteLine("\tMarca: {0}", marca);
+            Console.WriteLine("\tModelo: {0}", modelo);
+            Console.WriteLine("\tRodada: {0}", rodada);
+            Console.WriteLine("\tVelocidades: {0}", velocidades);
+            Console.WriteLine("\tColor: {0}", color);
+        }
     }
+
 }
